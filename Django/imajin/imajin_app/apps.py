fc11f8dbc120fab0f573fd96e809e145d357fd27
paran000ia <reyodzaki20100612@gmail.com>
2026-01-14 20:07:58 +0500

    sign

---
+++ Django/imajin/imajin_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ImajinAppConfig(AppConfig):
+    name = 'imajin_app'
---
+++ Django/imajin/imajin_app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, redirect
+
+def sign_up(request):
+    if request.meth
+    return render(request, "sign_up.html")
+def log_in(request):
+    pass
+def contact_us(request):
+    pass
+def coming_soon(request):
+    pass
+def not_found(request):
+    pass
